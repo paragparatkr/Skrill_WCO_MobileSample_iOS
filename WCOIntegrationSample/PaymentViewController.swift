002a60afe10e9423c1d8bc65f8876bee4e507e7c
Parag <46120039+paragparatkr@users.noreply.github.com>
2023-08-14 18:49:04 +0100

    Update PaymentViewController.swift

--- WCOIntegrationSample/PaymentViewController.swift
+++ WCOIntegrationSample/PaymentViewController.swift
@@ -33,7 +33,18 @@ final class PaymentViewController: UIViewController {
 
     @objc private func didTapButton() {
         /// skrillWalletCheckoutURL = This is the URL which is returned in paymentHandles API response (redirect_payment). iOS clients will open this url.
+        ///
+        /// Please note that you have to pass ``return_url`` parameter in "return_link" in paymenthandles call!!!
+        ///
+        /// You app should support Universal Links in order to continue with the deeplinking.
+        /// More information for Universal links can be found here:
+        /// https://developer.apple.com/library/archive/documentation/General/Conceptual/AppSearch/UniversalLinks.html
+        /// and better overview for app side support of universal links
+        /// https://developer.apple.com/documentation/xcode/supporting-universal-links-in-your-app
+
         
+        let skrillWalletCheckoutURL = "https://api.test.paysafe.com/alternatepayments/v1/redirect?accountId=1001461040&paymentHandleId=c181357e-601b-4b54-b698-f5c4be63ce4a&token=eyJhbGciOiJIUzI1NiJ9.eyJhY2QiOiIxMDAxNDYxMDQwIiwicHlkIjoiYzE4MTM1N2UtNjAxYi00YjU0LWI2OTgtZjVjNGJlNjNjZTRhIiwiZXhwIjoxNjkyMDM3MDI1fQ.rJWmngVyyz37sAxVcCYK934OjCGlDlcdeqiUEeIAfU8"
+
         guard let url = URL(string: "skrillWalletCheckoutURL") else {
             return
         }
